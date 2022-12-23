55fad489b87990fd426ee55dd166d0247d63c472
Nohelia2412 <121258730+Nohelia2412@users.noreply.github.com>
2022-12-23 06:30:59 -0500

    Update 7_🗂_Validación.py

--- pages/7_🗂_Validación.py
+++ pages/7_🗂_Validación.py
@@ -1,18 +1,7 @@
 import streamlit as st
 
-  st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-  st.markdown ( "<h2 style='text-align: center; color: black;'>Validación de Resultados</h2>" , unsafe_allow_html = True )
-  st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-  st.markdown ( "<h2 style='text-align: ; color: black;'>Validacion-Matrix de Correlacion</h2>" , unsafe_allow_html = True )
-  st.markdown ( 'Se debe llenar la tabla de VALIDACIÓN de la Matriz de Correlación con los valores de Similitud obtenidos En `NUMPY` a partir de las matrices nym con funciones. Se realiza la validación de los resultados obtenidos con la Matriz de Correlación de Pearson en Entumecido.' )
-  st.markdown ( "<h2 style='text-align: ; color: black;'>Validación de Resultados de Pandas</h2>" , unsafe_allow_html = True )
-  st.markdown ( 'Valores de Similitud en pandas' )
-  data1 =  pd.read_csv ( 'ENCUESTA.csv' )
-  dataframe1  =  pd.dataframe ( data )
-  lista1  =  dataframe1 [ 'Dirección de correo electrónico' ]. tolista ()
-
-
 
 st.write(""" ## VALIDACIÓN DE GRÁFICA DE CALOR """)
-st.write(""" ![https://scontent.faqp2-1.fna.fbcdn.net/v/t39.30808-6/321099572_1346330802807205_1098137571726666278_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGThkcpoboG--4wRn__tlNAY1I3LkboesRjUjcuRuh6xFyawAFnMEVR2Cqdz1hV1xzWjjIPnQP6D4j2TGfoNnWt&_nc_ohc=CbDIkOSUboMAX-CwkZH&_nc_ht=scontent.faqp2-1.fna&oh=00_AfB2WGKTqGVswHocXcUW7oTD3pZ9UKwDRbDw6GXDKW35lg&oe=63AA2F26) ] """)
+st.write(""" ![imagen2.png](https://scontent.faqp2-1.fna.fbcdn.net/v/t39.30808-6/321099572_1346330802807205_1098137571726666278_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGThkcpoboG--4wRn__tlNAY1I3LkboesRjUjcuRuh6xFyawAFnMEVR2Cqdz1hV1xzWjjIPnQP6D4j2TGfoNnWt&_nc_ohc=CbDIkOSUboMAX8hPrLp&_nc_ht=scontent.faqp2-1.fna&oh=00_AfC4PQALm1uRiYpakveqfqO8bTB6-6j0dfK0E8qPY2wOvA&oe=63AA2F26) """)
+
 
