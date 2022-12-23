0de987a381e3bf3bec57c73131ef2710900b33bb
Nohelia2412 <121258730+Nohelia2412@users.noreply.github.com>
2022-12-23 10:43:50 -0500

    Update 2_🗂_Caratula.py

--- pages/2_🗂_Caratula.py
+++ pages/2_🗂_Caratula.py
@@ -1,53 +1,27 @@
-import  streamlit as st  
-import  pandas  as  pd
-import  numpy as np  
-
-lista  = [ 'fbautistaca@unsa.edu.pe' ]
+import streamlit as st
 
 
-inicio  =  st.barra lateral botón ( 'Inicio' )
-objetivos  =  st.barra lateral botón ( 'Objetivos' )
-base_teorica  =  st.barra lateral botón ( 'Base Teórica' )
-propuesta  =  st.barra lateral botón ( 'Propuesta' )
-correlacion_de_pearson  =  st.barra lateral boton ( 'Correlacion de Pearson' )
-nuestra_correlacion  =  st.barra lateral boton ( 'Nuestra Correlacion' )
-mapa_de_calor  =  calle.barra lateral botón ( 'Mapa de Calor' )
-validacion_de_resultados  =  st.barra lateral botón ( 'Validación de Resultados' )
-conclusiones  =  st.barra lateral botón ( 'Conclusiones' )
-st.barra lateral imagen ( 'https://upload.wikimedia.org/wikipedia/commons/3/3a/LOGO_UNSA.png' )
-st.nieve ()
-st.globos ()
-si  inicio :
-    st.nieve ()
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Universidad Nacional de San Agustín de Arequipa </h2>" , unsafe_allow_html = True )
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Escuela Profesional de Ingeniería de Telecomunicaciones </h2>" , unsafe_allow_html = True )
+st.markdown("<h1 style='text-align: center; color: black;'>Universidad Nacional de San Agustín de Arequipa</h1>", unsafe_allow_html=True)
+ 
+st.markdown("<h1 style='text-align: center; color: black;'>Escuela Profesional de Ingeniería de Telecomunicaciones</h1>", unsafe_allow_html=True)
 
-    st.imagen ( 'https://www.unsa.edu.pe/wp-content/uploads/sites/3/2018/05/Logo-UNSA.png' )
+st.write(""" ![image.png](https://www.unsa.edu.pe/ouresponsabilidadsocial/wp-content/themes/observatorio/img/unsa-logo.png) """)
+st.write("""## Ingeniero Renzo Bolivar - Docente DAIE""")
+st.write("""Curso : Computación 1""")
 
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Ingeniero Renzo Bolivar - Docente DAIE</h2>" , unsafe_allow_html = True )
+st.write("""# ![linea 1](https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png)""")
 
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Curso : Computación 1 </h2>" , unsafe_allow_html = True )
+st.write("""## GRUPO A """)
+st.write("""## Alumnos:""")  
+st.write("""
+- Chambilla Cayo Nohelia Kathia
+""") 
 
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png' )
+st.write("""# ![linea 1](https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png)""")
 
-    st.markdown ( "<h2 style='text-align: center; color: black;'>GRUPO A - Nº4 </h2>" , unsafe_allow_html = True )
+st.write("""## INVESTIGACIÓN FORMATIVA""") 
+st.write("""## PROYECTO FINAL""")
+st.write("""## PYTHON - Inteligencia Artificial""")
 
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Alumnos:</h2>" , unsafe_allow_html = True )
+st.write("""# ![linea 1](https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png)""")
 
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Alanoca Maquera, Brandon Yoel</h2>" , unsafe_allow_html = True )   
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Bautista Cari, Fabrizio Miguel</h2>" , unsafe_allow_html = True )
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Sarcco Yana Jose, Fabricio</h2>" , unsafe_allow_html = True )
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Pacco Chua Yaseth, Yeremi</h2>" , unsafe_allow_html = True )           
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Miranda Cahuana, Francisco Alonso</h2>" , unsafe_allow_html = True )
-    st.markdown ( "<h2 style='text-align: center; color: black;'>Yanqui Calcina, Hierald Farid</h2>" , unsafe_allow_html = True )
-
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png' )
-
-    st.markdown ( "<h2 style='text-align: center; color: black;'>INVESTIGACIÓN FORMATIVA</h2>" , unsafe_allow_html = True )
-  
-    st.markdown ( "<h2 style='text-align: center; color: black;'>PYTHON - Inteligencia Artificial</h2>" , unsafe_allow_html = True )
-    
-    st.imagen ( 'https://s1.eestatic.com/2020/08/19/invertia/empresas/energia/transicion_energetica-inteligencia_artificial-energias_renovables_514209961_158012153_1706x960.jpg' )
-    
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926252-2b8a0c00-a448-11eb-9d9c-b43beaf0ff68.png' )
-    
