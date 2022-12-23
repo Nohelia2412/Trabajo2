3af808c1be1704b473ffc8fa5a4366c055639926
Nohelia2412 <121258730+Nohelia2412@users.noreply.github.com>
2022-12-23 10:51:34 -0500

    Update 7_🗂_Validación.py

--- pages/7_🗂_Validación.py
+++ pages/7_🗂_Validación.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 
 st.write(""" ## VALIDACIÓN DE GRÁFICA DE CALOR """)
+st.write("""### Grafica de calor por correlacion de pandas""")
+st.write(""" ![imagen2.png]("""https://scontent.faqp2-1.fna.fbcdn.net/v/t39.30808-6/321675318_717252202968243_2569053303383691197_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEp9yH_CLdoWx0JxCQD85nTfs03RmKri0R-zTdGYquLROkMmyuHseJ2IaxFcvb8XV9OjFiARttfxMWdFHUJOC7h&_nc_ohc=5llMYqvesXsAX8h6wxY&_nc_ht=scontent.faqp2-1.fna&oh=00_AfAhBka5As6vp8bYg5ORvMqVk2kQ6jdnspF8Ujy6dJefXg&oe=63AA0BAC""")
+
+st.write("""### Grafica de calor por la nueva correlacion""")
 st.write(""" ![imagen2.png](https://scontent.faqp2-1.fna.fbcdn.net/v/t39.30808-6/321099572_1346330802807205_1098137571726666278_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGThkcpoboG--4wRn__tlNAY1I3LkboesRjUjcuRuh6xFyawAFnMEVR2Cqdz1hV1xzWjjIPnQP6D4j2TGfoNnWt&_nc_ohc=CbDIkOSUboMAX8hPrLp&_nc_ht=scontent.faqp2-1.fna&oh=00_AfC4PQALm1uRiYpakveqfqO8bTB6-6j0dfK0E8qPY2wOvA&oe=63AA2F26) """)
 
 
