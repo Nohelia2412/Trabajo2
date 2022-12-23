ad240d26655eb142d67e760b9bbb815e178531d3
Nohelia2412 <121258730+Nohelia2412@users.noreply.github.com>
2022-12-23 06:01:48 -0500

    Update 8_🗂_Conclusiones.py

--- pages/8_🗂_Conclusiones.py
+++ pages/8_🗂_Conclusiones.py
@@ -1,29 +1,28 @@
 import streamlit as st
 
-conclusiones:
+import streamlit as st
 
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )  
-    st.markdown ( "<h2 style='text-align: center ; color: black;'>Conclusiones</h2>" , unsafe_allow_html = True )
-    
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-    st.markdown ( "<h2 style='text-align: ; color: black;'>¿Se valido o no los resultados?</h2>" , unsafe_allow_html = True )
-    st.markdown ( 'Se realizo un formulario en google, para despues ser convertidos en un archivo CSV y utilizarlos conjuntamente con las librerias de data science Pandas, Seaborn, Matplotlib y Numpy. ' )
-    
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-    st.markdown ( "<h2 style='text-align: ; color: black;'>¿Es efectivo el método de conexión de pearson?</h2>" , unsafe_allow_html = True )
-    st.markdown ( 'La correlacion de pearson fue eficiente para calcular la correlacion existente entre los datos que teniamos , estos valores necesitan ser de manera cuantitativa expresados ​​en valores numericos. Por los tanto podemos decir que el metodo de correlacion de Pearson si es efectivo.')
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-    st.markdown ( "<h2 style='text-align: ; color: black;'>Los resultados Validados son:</h2>" , unsafe_allow_html = True )
-    st.markdown ( 'Segun los resultados que fueron validos con Pandas y nuestro algoritmo los datos validados seria' )
-    st.markdown ( '1. noeliaparedesgu@gmail.com y gparedesg@unsa.edu.pe obtienen el PRIMER indice mas alto de similitud' )
-    st.markdown ( '2. noeliaparedesgu@gmail.com y elhuamani@unsa.edu.pe obtienen el SEGUNDO indice mas alto de similitud' )
-    
-    
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-    st.markdown ( "<h2 style='text-align: ; color: black;'>Correlación de Pearson y Regresión Lineal, ¿cual es su relación?</h2>" , unsafe_allow_html = True )
-    st.markdown ( 'La correlación de Pearson, es el que mide la regresión lineal de los datos obtenidos, por lo tanto la regresión lineal es parte de la correlación de pearson. Siendo la correlación de pearson el método y la regresión lineal el resultado.' )
-    
-    
-    st.imagen ( 'https://user-images.githubusercontent.com/19308295/115926262-2fb62980-a448-11eb-8189-c2f10e499944.png' )
-    
+st.write("""###  ¿Se valido o no los resultados? """)
+
+st.write("""Se realizo un formulario en google, para despues ser convertidos en un archivo CSV y utilizarlos conjuntamente con las librerias de data science Pandas, Seaborn, Matplotlib y Numpy.etc. """)
+
+st.write("""###  Los resultados validados son: """)
+st.write("""Segun los resultados que fueron validos con Pandas y nuestro algoritmo los datos validados seria: 
+    1. noeliaparedesgu@gmail.com y gparedesg@unsa.edu.pe obtienen el PRIMER indice mas alto de similitud.
+    2. noeliaparedesgu@gmail.com y elhuamani@unsa.edu.pe obtienen el SEGUNDO indice mas alto de similitud.'""")
+
+st.write("""###   ¿Es efectivo el método de correlación de Pearson?""")
+st.write("""La correlacion de pearson fue eficiente para calcular la correlacion existente entre los datos que teniamos , estos valores necesitan ser de manera cuantitativa expresados en valores numericos. Por los tanto podemos decir que el metodo de correlacion de Pearson si es efectivo.""")
+
+st.write("""###   Correlación de Pearson y Regresión Lineal, ¿cuál es su relación? """)
+st.write("""La correlación de Pearson, es el que mide la regresión lineal de los datos obtenidos, por lo tanto la regresión lineal es parte de la correlación de pearson. Siendo la correlación de pearson el método y la regresión lineal el resultado. )""")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+
 
